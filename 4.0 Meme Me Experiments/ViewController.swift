72b1d95bda9636b6d6d68f8e1399a5afe03a4e18
EvB <vonbod@gmail.com>
2018-09-29 10:01:30 +0200

    Initial Commit

---
+++ 4.0 Meme Me Experiments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4.0 Meme Me Experiments
+//
+//  Created by Enno von Bodecker on 29.09.18.
+//  Copyright © 2018 EvB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
